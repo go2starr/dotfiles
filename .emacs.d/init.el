cbd6b7d28c344643164a60c5c4e202e7c07e3259
Mike Starr <mstarr@fb.com>
2012-11-07 15:37:34 -0800

    Add package manager to emacs.d

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,3 +1,19 @@
+;; Load-path
+(add-to-list 'load-path "~/.emacs.d/")
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; FB master emacs
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defvar master-dir (getenv "ADMIN_SCRIPTS"))
 (load-library (concat master-dir "/master.emacs"))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Package manager
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(require 'package)
+;; Any add to list for package-archives (to add marmalade or melpa) goes here
+(add-to-list 'package-archives
+    '("marmalade" .
+      "http://marmalade-repo.org/packages/"))
+(package-initialize)
