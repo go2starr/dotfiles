e8ea4604f291f5c12cdb08227a62d10f91e75460
Mike Starr <mstarr@fb.com>
2012-11-08 22:01:45 -0800

    Revert to symbolic TAGS..

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -28,8 +28,8 @@
 (load-library (concat master-dir "/master.emacs"))
 
 ;; Tags
-(let (large-file-warning-threshold 'nil) (visit-tags-table "/home/engshare/pfff/TAGS")) ; Really large file
-(visit-tags-table "/home/engshare/pfff/TAGS")
+(let (large-file-warning-threshold 'nil) (visit-tags-table "~/www/TAGS")) ; Really large file
+(visit-tags-table "~/www/TAGS")
 
 ;; Autocomplete
 (load "/home/engshare/tools/pfff_php")
